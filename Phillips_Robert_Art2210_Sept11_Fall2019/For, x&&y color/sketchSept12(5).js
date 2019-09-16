5ca523a7c88d78fe4587e3944f59de64820367a0
Robert Phillips <rphil36@lsu.edu>
2019-09-16 12:29:30 -0500

    Update sketchSept12(5).js

--- Phillips_Robert_Art2210_Sept11_Fall2019/For, x&&y color/sketchSept12(5).js
+++ Phillips_Robert_Art2210_Sept11_Fall2019/For, x&&y color/sketchSept12(5).js
@@ -5,13 +5,11 @@ function setup(){
   
 }
     
-function ellipseTime(){
+function ellipseTime(r,g,b){
     for(var x = 0; x <= windowWidth; x+=50){  
         for(var y = 0; y <= windowHeight; y+=50){  
-            if((x%75==0)||(y%75==0)){
-            let r = random(0,255);
-            let g = random(0,255);
-            let b = random(0,255);
+            if((x%75==0)&&(y%75==0)){
+            
              ellipse(x,y, 30, 30);
              fill(r, g, b);
             }
@@ -20,7 +18,10 @@ function ellipseTime(){
 }
 function draw(){
     background(0,50,50);
-    ellipseTime();
+    let r = random(0,255);
+    let g = random(0,255);
+    let b = random(0,255);
+    ellipseTime(r,g,b);
 
     
 }
